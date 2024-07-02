a6c8f6da5d997d5a9bbd17e1f598b7ecbd7a7ca4
Charles <charlesbruno2010@hotmail.com>
2024-07-02 20:52:27 -0300

    feat matérias, faltas e  resultado

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ Selecione o menu desejado:
 1 - Cadastro de aluno.
 2 - Listagem de alunos.
 3 - Cadastro de materia.
-4 - Cadastro de notas.
-5 - Conferencia de media.
+4 - Conferencia de resultados.
 0 - Encerrar aplicacao.
 `)
         let menu = prompt("");
@@ -24,7 +23,7 @@ Selecione o menu desejado:
                 const nome = prompt(`Escreva o nome do aluno ${index+1}: `);
                 const aluno = {
                     nome: nome,
-                    materias:[],
+                    materias: [],
                 }
                 alunos.push(aluno);
             }
@@ -38,29 +37,82 @@ Selecione o menu desejado:
             let chaveMaterias = true;
             if(alunos.length>0){
                 for (let index = 0; index < alunos.length; index++) {
-                    console.log(`1-${alunos[index].nome}.`)
+                    console.log(`${index + 1}-${alunos[index].nome}.`)
                 }
                 
-                const alunoIndexStr = prompt(`Qual aluno deseja cadastrar materia? `);
+                const alunoIndexStr = prompt(`Digite o numero do aluno que deseja cadastrar a matéria? `);
                 const alunoIndex = parseInt(alunoIndexStr) - 1;
 
                 if (isNaN(alunoIndex) || alunoIndex < 0 || alunoIndex >= alunos.length){
                     console.log("Aluno nao encontrado.")
                 }else{
-                    console.log("Escreva o nome da materia ou digite 'sair' para voltar ao menu principal. ")
+                    let numeroDeMaterias = 0;
+                    console.log("Escreva o nome da matéria ou digite 'sair' para voltar ao menu principal. ")
                     
-                    while(chaveMaterias){
+                    while (numeroDeMaterias < 3 || chaveMaterias) {
                         const nomeMateria = prompt("");
-                        if (nomeMateria.toLowerCase() === "sair"){
+                        if (nomeMateria.toLowerCase() === "sair" && numeroDeMaterias >= 3) {
                             chaveMaterias = false;
+                        } else if (nomeMateria.toLowerCase() === "sair" && numeroDeMaterias < 3) {
+                            console.log("Você deve cadastrar no mínimo 3 matérias!");
+                            console.log("Escreva o nome da matéria: ")
                         } else {
                             const materia = {
                                 nome: nomeMateria,
-                                notas: []
+                                notas: [],
+                                faltas: 0
                             }
+
+                            const faltas = parseInt(prompt(`Quantas faltas ${alunos[alunoIndex].nome} teve? `));
+                            materia.faltas = faltas;
+                            
+                            console.log("Faca o cadastro das 3 notas!")
+                            for (let index = 0; index < 3; index++) {
+                                let nota;
+                                do {
+                                    nota = parseFloat(prompt(`Digite a ${index + 1} nota (0 a 10): `));
+                                    if (nota < 0 || nota > 10) {
+                                        console.log("Nota inválida! Digite uma nota entre 0 e 10.");
+                                    }
+                                } while (nota < 0 || nota > 10);
+                                materia.notas.push(nota);
+                            }
+
                             alunos[alunoIndex].materias.push(materia);
+                            numeroDeMaterias++;
                             console.log(`Matéria '${nomeMateria}' cadastrada para o aluno '${alunos[alunoIndex].nome}'.`);
-                            console.log("Escreva o nome da materia ou digite 'sair' para voltar ao menu principal. ")
+
+                            console.log("Escreva o nome da matéria ou digite 'sair' para voltar ao menu principal. ")
+                        }
+                    }
+                }
+            }else{
+                console.log("Nenhum aluno cadastrado.")
+            }
+        }if(menu == 4){
+            if(alunos.length > 0){
+                for (let index = 0; index < alunos.length; index++) {
+                    console.log(`Aluno: ${alunos[index].nome}`);
+                    for (let indexMat = 0; indexMat < alunos[index].materias.length; indexMat++) {
+                        let materia = alunos[index].materias[indexMat];
+                        let somaNotas = 0;
+                        if (materia.faltas > 5){
+                            console.log(
+`Matéria: ${materia.nome}, 
+Resultado: Reprovado por falta!
+`
+                        );
+                        }else{
+                            for (let i = 0; i < materia.notas.length; i++) {
+                                somaNotas += materia.notas[i];
+                            }
+                            let media = somaNotas / materia.notas.length;
+                            console.log(
+`Matéria: ${materia.nome}, 
+Média: ${media.toFixed(2)}, 
+Resultado: ${media.toFixed(2)>=7?'Aprovado, acima da media!':'Reprovado, abaixo da media!'}
+`
+                            );
                         }
                     }
                 }
